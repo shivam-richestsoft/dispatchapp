1abae11897f9a4b8a3862e809277c74f65dc8921
Guri <gurvindar.s@richestsoft.in>
2021-08-26 15:57:30 +0530

    api base

---
+++ app/Http/Controllers/api/UserController.php
@@ -0,0 +1,247 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+
+//facades
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Paginate;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Arr;
+
+
+//models
+use App\Models\User;
+use App\Models\AppLogin;
+use App\Models\EmploymentHistory as EH;
+use App\Models\Project;
+use App\Models\Socialmedia;
+use App\Models\Language;
+use App\Models\Skill;
+use App\Models\UserSkill;
+use App\Models\PasswordReset;
+use App\Models\Expertise;
+use App\Models\FriendRequest;
+use App\Models\FriendList;
+use App\Models\Post;
+use App\Models\Notification;
+use App\Models\File;
+use App\Models\Favourite;
+use App\Models\Thumbsup;
+use App\Models\SinglePostFav;
+use App\Models\SinglePostThumbs;
+use App\Models\FileReaction;
+use App\Models\PostReaction;
+use App\Models\Comment;
+use App\Models\Reply;
+use App\Models\FileComment;
+use App\Models\FileCommentReply;
+use App\Models\PostViews;
+use App\Models\FileViews;
+
+//additional
+use DB;
+use Carbon\Carbon;
+use Validator;
+use Session;
+
+//traits
+use App\Traits\ApiResponser;
+use App\Traits\ImageUpload;
+use App\Traits\Email;
+use App\Traits\Togglestatus;
+
+//requests
+use App\Http\Requests\UserRequest;
+
+//events
+use App\Events\Notify;
+
+
+class UserController extends Controller
+{
+    use ApiResponser;
+    use ImageUpload;
+    use Email;
+    use Togglestatus;
+
+    // showing error when user not logged in    
+    public function loginCheck(request $r)
+    {
+        try {
+            return $this->success(false, 'Please login to access this page', 403);
+        } catch (\Exception $e) {
+            return $this->error('Please check your fields');
+        }
+    }
+    //logout current device    
+    public function logout(request $r)
+    {
+        try {
+            $r->user()->currentAccessToken()->delete();
+            return $this->success(true, 'Successfully loggged out');
+        } catch (\Exception $e) {
+            return $this->error('Please check your fields');
+        }
+    }
+    // logout all device
+    public function logoutAll()
+    {
+        try {
+            auth()->user()->tokens()->delete();
+            return $this->success(true, 'Successfully loggged out all devices');
+        } catch (\Exception $e) {
+            return $this->error('Please check your fields');
+        }
+    }
+
+    // register for business and individual
+    public function register(Request $r)
+    {
+        try {
+            $register = User::create(
+                [
+                    'name' => $r->name,
+                    'email' => $r->email,
+                    'phone' => $r->phone,
+                    'password' => Hash::make($r->password)
+                ]
+            );
+            return $this->success(true, 'Registration successful');
+        } catch (\Throwable $e) {
+            return $this->error($e->getMessage());
+        }
+    }
+
+    //update profile for both users
+    public function updateProfile(UserRequest $r)
+    {
+        try {
+            $user = Auth::user();
+            $update = User::where('id', $user->id)
+                ->update([
+                    'name' => $r->name, 'email' => $r->email,
+                    'phone' => $r->phone, 'business_name' => $r->business_name
+                ]);
+            return $this->success(true, 'Profile updated successfully');
+        } catch (\Throwable $e) {
+            return $this->error('Please check your fields');
+        }
+    }
+    //login for both
+    public function login(request $r)
+    {
+        try {
+            $login = $r->validate([
+                'email' => 'required',
+                'password' => 'required',
+            ]);
+            if (Auth::attempt($login)) {
+                $user = auth()->user();
+                $first_time_login = false;
+                if (auth()->user()->first_time_login == self::FIRST_LOGIN_FALSE) {
+                    $first_time_login = true;
+                    $user->update(['first_time_login' => self::FIRST_LOGIN_TRUE]);
+                }
+                $token = $user->createToken('API Token')->plainTextToken;
+                $profileDetails['is_business'] = $business_status ?? false;
+                $profileDetails['is_individual'] = $user_status ?? false;
+                $business_status = false;
+                $user_status = false;
+                $user->is_business == 1 ? $business_status = true : $user_status = true;
+                $list = [];
+                $list['token'] =  $token;
+                $list['first_time_login'] =  $first_time_login;
+                $list['name'] =  $user->name;
+                $list['profile_image'] =  $user->profile_image;
+                $list['is_business'] = $business_status ?? false;
+                $list['is_individual'] = $user_status ?? false;
+                return $this->successWithData($list);
+            } else {
+                return $this->error('Invalid credentials');
+            }
+        } catch (\Throwable $e) {
+            return $this->error($e->getMessage());
+        }
+    }
+    //get all profile including additional info of both users
+    public function getProfile(request $r)
+    {
+
+        $profileDetails = [];
+        $languages = [];
+        $skills = [];
+        try {
+            $data = User::profileResponse(auth()->user()->id);
+            if (!empty($data)) {
+                return $this->successWithData($data);
+            } else {
+                return $this->success(false);
+            }
+        } catch (\Throwable $e) {
+            return $this->error($e->getMessage());
+        }
+    }
+
+    // forget password 
+    public function forgotPassword(request $r)
+    {
+        $v = Validator::make(
+            $r->input(),
+            [
+                'email' => 'required|email|max:255',
+            ]
+        );
+        if ($v->fails()) {
+            return $this->validation($v);
+        }
+        try {
+            $user = User::where('email', $r->email)->first();
+            if (!empty($user)) {
+                $token = Str::random(20);
+                $updated = PasswordReset::updateOrCreate(['email' => $r->email], ['email' => $r->email, 'token' => $token]);
+                if ($this->sendResetEmail($user, $token)) {
+                    return $this->success(true, 'Password reset link has been sent to your registered email id.');
+                } else {
+                    return $this->success(false);
+                }
+            } else {
+                return $this->success(false, 'This email is not registered yet');
+            }
+        } catch (\Throwable $e) {
+            return $this->error($e->getMessage());
+        }
+    }
+    // changePassword
+    public function changePassword(request $r)
+    {
+        $v = Validator::make(
+            $r->input(),
+            [
+                'token' => 'required',
+                'password' => 'required',
+            ]
+        );
+        if ($v->fails()) {
+            return $this->validation($v);
+        }
+        $token = $r->token;
+        $email = PasswordReset::where('token', $r->token)->pluck('email')->first();
+        if (empty($email)) {
+            return $this->success(false, 'You have not requested for password change');
+        } else {
+            $updated = User::where('email', $email)->update(['password' => md5($r->password)]);
+            if ($updated) {
+                PasswordReset::where('token', $r->token)->delete();
+                return $this->success(true, 'Password changed successfully');
+            } else {
+                return $this->success(false);
+            }
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\UserController;
 
 
 /*
@@ -19,4 +20,125 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('/withdrawRequest/{id}/{amount}','App\Http\Controllers\api\AdminController@withDrawRequest');
+
+Route::post('register',[UserController::class, 'register']);
+Route::post('login',[UserController::class, 'login']);
+Route::post('forgot-password',[UserController::class, 'forgotPassword']);
+Route::post('change-password',[UserController::class, 'changePassword']);
+
+//to show error when user not logged in --- used in middleware(Authenticate)
+Route::get('login-check',[UserController::class, 'loginCheck'])->name('login-check');
+
+Route::middleware('auth:sanctum')->group( function () {
+
+//invite    
+// Route::post('invite',[UC::class, 'invite']);
+
+// //general
+// Route::get('get-profile',[UC::class, 'getProfile']);
+// Route::post('update-profile',[UC::class, 'updateProfile']);
+// Route::post('additional-info',[UC::class, 'additionalInfo']);
+
+// //lists
+// Route::get('language-list',[UC::class, 'languageList']);
+// Route::get('skills-list',[UC::class, 'skillsList']);
+// Route::get('expertise-list',[UC::class, 'expertiseList']);
+
+// //Adding project for business
+// Route::post('add-projects',[UC::class, 'addProjects']);
+// Route::post('update-project',[UC::class, 'updateProject']);
+// Route::post('delete-project',[UC::class, 'deleteProject']);
+// Route::get('projects-list',[UC::class, 'projectsListing']);
+
+// //Adding employment for indvidual
+// Route::post('add-employment',[UC::class, 'addEmployment']);
+// Route::post('update-employment',[UC::class, 'updateEmployment']);
+// Route::post('delete-employment',[UC::class, 'deleteEmployment']);
+// Route::get('employment-list',[UC::class, 'employmentsListing']);
+
+// //social media
+// Route::post('add-social',[UC::class, 'addSocial']);
+// Route::get('get-social',[UC::class, 'getSocial']);
+
+// // logout current device
+// Route::get('logout',[UC::class, 'logout']);
+
+// //logout all devices
+// Route::get('logout-all',[UC::class, 'logoutAll']);
+
+// //search
+// Route::post('search-user',[UC::class, 'searchUser']);
+
+// //friend request
+// Route::post('send-request',[UC::class, 'sendRequest']);
+// Route::post('delete-request',[UC::class, 'deleteRequest']);
+// Route::get('pending-request',[UC::class, 'pendingRequest']);
+// Route::get('sent-request',[UC::class, 'sentRequest']);
+// Route::post('accept-request',[UC::class, 'acceptRequest']);
+// Route::post('cancel-request',[UC::class, 'cancelRequest']);
+// Route::post('unfriend',[UC::class, 'unfriend']);
+
+// //get additiona details i.e emploment history or prjoject history and social links
+// Route::post('get-additional-details',[UC::class, 'getAdditionalDetails']);
+
+// //notifications
+// Route::get('notifications',[UC::class, 'notifications']);
+
+// //post
+// Route::post('post-something',[UC::class, 'postSomething']);
+
+// //post
+// Route::get('friends-list',[UC::class, 'friendsList']);
+
+// //timeline
+// Route::get('timeline',[UC::class, 'timeline']);
+
+// //Get Post 
+// Route::get('getpost',[UC::class,'getpost']);
+// Route::post('getfile',[UC::class,'GetFile']);
+// //Notification Onscreen 
+// Route::get('onScreenNotification',[UC::class,'onScreenNotification']);
+
+// //Read Notification
+// Route::post('readnotificaton',[UC::class,'readnotificaton']);
+
+// //Like and Dislike
+// Route::post('likedislike',[UC::class,'LikeAndDislike']);
+// Route::post('singlelikedislike',[UC::class,'FilesLikeAndDislike']);
+
+// //Thumbs Up AND Thumbs Down
+// Route::post('thumbsupdown',[UC::class,'ThumbsUpandDown']);
+// Route::post('singlethumbsupdown',[UC::class,'FilesThumbsUpandDown']);
+
+// //Reaction Listing
+// Route::post('likelisting',[UC::class,'LikeListing']);
+// Route::post('singlelikelisting',[UC::class,'SingleLikeListing']);
+
+// //Reaction
+// Route::post('postreaction',[UC::class,'PostReact']);
+// Route::post('singlepostreaction',[UC::class,'FileReact']);
+
+// //Reaction Listing
+// Route::post('reactionlisting',[UC::class,'ReactionListing']);
+// Route::post('singlereactionlisting',[UC::class,'SingleReactionListing']);
+
+// //Comments
+// Route::post('postcomment',[UC::class,'PostComment']);
+// Route::post('postcommentreply',[UC::class,'PostCommentReply']);
+
+// //Comments Listing
+// Route::post('postcommentlisting',[UC::class,'PostCommentListing']);
+// Route::post('postcommentreplylisting',[UC::class,'PostCommentReplyListing']);
+
+// //File Comments
+// Route::post('filecomment',[UC::class,'FileComment']);
+// Route::post('filecommentlisting',[UC::class,'FileCommentListing']);
+
+// //File Comments Reply
+// Route::post('filecommentreply',[UC::class,'FileCommentReply']);
+// Route::post('filecommentreplylisting',[UC::class,'FileCommentReplyListing']);
+
+// //Post View Count Increment
+// Route::post('postviewed',[UC::class,'PostViewCount']);
+// Route::post('fileviewed',[UC::class,'FileViewCount']);
+});
